a47e6c64bf8c0e1ed56d807167d718dad5498fea
sajalkhan <sajalhossen@yahoo.com>
2021-04-04 14:26:16 +0600

    learn use of computed property

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
-  <div :class="['divStyle']">
-    <ul v-for="(dist, indx) in district" :key="indx" :class="['list']">
-      <li>{{ dist }}</li>
-    </ul>
+  <div>
+    <h4>full name: {{ firstName }} {{ lastName }}</h4>
+    <h4>full name with computer property: {{ fullName }}</h4>
+    <h4>
+      Total -
+      {{ listItem.reduce((Total, curr) => (Total += curr.price), 0) }}
+    </h4>
+    <h4>Calculate total with computed property: {{ totalPrice }}</h4>
+
+    <!-- show value which is greater than 1000 -->
+    <template v-for="item in listItem" :key="item.id">
+      <h4 v-if="item.price > 1000">{{ item.name }} - {{ item.price }}</h4>
+    </template>
+
+    <hr />
+    <!-- show expensive item using computed property -->
+    <h4>Using computer property</h4>
+    <div v-for="item in expensiveItems" :key="item.id">
+      <h4>{{ item.name }} - {{ item.price }}</h4>
+    </div>
+    <hr />
   </div>
 
-  <div v-for="(u, indx) in users" :key="indx">
-    <h2>Name: {{ u.name }}</h2>
-    <span v-for="(info, i) in u.info" :key="i">
-      <label v-if="i === 0"><strong>Home Town:</strong> {{ info }}, </label>
-      <label v-if="i === 1"><strong>Home Dist:</strong> {{ info }}, </label>
-      <label v-if="i === 2"><strong>University:</strong> {{ info }}</label>
-    </span>
+  <!-- use of computed property setter -->
+  <div>
+    <input
+      type="text"
+      @keyup.enter="changeName"
+      v-model.trim.lazy="userfullName"
+    />
+    <h4>computed full name: {{ computedFullName }}</h4>
   </div>
 </template>
 <script>
@@ -19,22 +37,60 @@ export default {
   name: "App",
   data() {
     return {
-      district: ["dhaka", "rajshahi", "natore", "khulna"],
-      users: [
+      firstName: "sajal",
+      lastName: "khan",
+      userfullName: "",
+      listItem: [
         {
-          name: "sohrab hossain",
-          info: ["natore", "rajshahi", "diu"],
+          id: 0,
+          name: "tv",
+          price: 1000,
         },
         {
-          name: "khairul bashar",
-          info: ["natore", "rajshahi", "hstu"],
+          id: 2,
+          name: "freez",
+          price: 3000,
         },
         {
-          name: "mustafizur rahaman",
-          info: ["rangpur", "rajshahi", "ru"],
+          id: 3,
+          name: "Ac",
+          price: 7000,
+        },
+        {
+          id: 4,
+          name: "computer",
+          price: 4000,
         },
       ],
     };
+  },
+  methods: {
+    changeName() {
+      const value = this.userfullName.split(" ");
+      this.firstName = value[0];
+      this.lastName = value[1];
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    computedFullName: {
+      get() {
+        return `${this.firstName} ${this.lastName}`;
+      },
+      set(value) {
+        const name = value.split(" ");
+        this.firstName = name[0];
+        this.lastName = name[1];
+      },
+    },
+    totalPrice() {
+      return this.listItem.reduce((total, curr) => (total += curr.price), 0);
+    },
+    expensiveItems() {
+      return this.listItem.filter((item) => item.price > 1000);
+    },
   },
 };
 </script>
