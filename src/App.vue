8906ff3c2434e731ff848e02cbd43a1f67f8acb0
sajalkhan <sajalhossen@yahoo.com>
2021-04-11 23:39:52 +0600

    learn how to  call functin from child to parent components

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,28 @@
 <template>
   <div :class="['divStyle']">
-    <ul v-for="(dist, indx) in district" :key="indx" :class="['list']">
-      <li>{{ dist }}</li>
-    </ul>
-  </div>
-
-  <div v-for="(u, indx) in users" :key="indx">
-    <h2>Name: {{ u.name }}</h2>
-    <span v-for="(info, i) in u.info" :key="i">
-      <label v-if="i === 0"><strong>Home Town:</strong> {{ info }}, </label>
-      <label v-if="i === 1"><strong>Home Dist:</strong> {{ info }}, </label>
-      <label v-if="i === 2"><strong>University:</strong> {{ info }}</label>
-    </span>
+    <button @click="show = true">Open Popup</button>
+    <Popup v-show="show" @closeModel="closePopup" />
+    <p>{{ childData }}</p>
   </div>
 </template>
 <script>
+import Popup from "./components/popup";
 export default {
   name: "App",
   data() {
     return {
-      district: ["dhaka", "rajshahi", "natore", "khulna"],
-      users: [
-        {
-          name: "sohrab hossain",
-          info: ["natore", "rajshahi", "diu"],
-        },
-        {
-          name: "khairul bashar",
-          info: ["natore", "rajshahi", "hstu"],
-        },
-        {
-          name: "mustafizur rahaman",
-          info: ["rangpur", "rajshahi", "ru"],
-        },
-      ],
+      show: false,
+      childData: "",
     };
+  },
+  methods: {
+    closePopup(value) {
+      this.show = false;
+      this.childData = value;
+    },
+  },
+  components: {
+    Popup,
   },
 };
 </script>
